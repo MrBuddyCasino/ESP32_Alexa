9682e52a39dfedafed53326ce66d11ce85abb734
Michael Böckling <michael@boeckling.net>
2017-05-23 13:22:30 +0200

    Initial import.

---
+++ components/sntp/include/sntp.h
@@ -0,0 +1,13 @@
+/*
+ * sntp.h
+ *
+ *  Created on: 23.05.2017
+ *      Author: michaelboeckling
+ */
+
+#ifndef _INCLUDE_SNTP_H_
+#define _INCLUDE_SNTP_H_
+
+void obtain_time(void);
+
+#endif /* _INCLUDE_SNTP_H_ */
---
+++ components/sntp/sntp.c
@@ -0,0 +1,56 @@
+/* LwIP SNTP example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "esp_log.h"
+#include "esp_attr.h"
+#include "esp_deep_sleep.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "apps/sntp/sntp.h"
+#include "sntp.h"
+
+
+static const char *TAG = "sntp";
+
+static void initialize_sntp(void)
+{
+    ESP_LOGI(TAG, "Initializing SNTP");
+    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    sntp_setservername(0, "pool.ntp.org");
+    sntp_init();
+}
+
+void obtain_time(void)
+{
+    initialize_sntp();
+
+    // wait for time to be set
+    time_t now = 0;
+    struct tm timeinfo = { 0 };
+    int retry = 0;
+    const int retry_count = 10;
+    while(timeinfo.tm_year < (2016 - 1900) && ++retry < retry_count) {
+        ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        time(&now);
+        localtime_r(&now, &timeinfo);
+    }
+
+    sntp_stop();
+}
+
