d9c3ee2dba9515c358bd0145c45a843900360de7
Michael Böckling <michael@boeckling.net>
2017-06-02 12:09:44 +0200

    Initial import.

---
+++ components/asio/include/asio_secure_socket.h
@@ -0,0 +1,14 @@
+/*
+ * asio_secure_socket.h
+ *
+ *  Created on: 02.06.2017
+ *      Author: michaelboeckling
+ */
+
+#ifndef _INCLUDE_ASIO_SECURE_SOCKET_H_
+#define _INCLUDE_ASIO_SECURE_SOCKET_H_
+
+/* create an encrypted socket connection */
+asio_connection_t *asio_new_ssl_connection(asio_registry_t *registry, asio_transport_t transport_proto, char *uri, int bidi, char *alpn, cipher_suite *suites, size_t num_suites, void *user_data);
+
+#endif /* _INCLUDE_ASIO_SECURE_SOCKET_H_ */
